2ddf25a4879942ea2bec1c53c286fb53712fe3af
Sebastian Ouellette <bigbrosebas@gmail.com>
2020-12-11 20:08:50 -0500

    Update RandomColourLoop.asm

--- RandomColourLoop.asm
+++ RandomColourLoop.asm
@@ -1,16 +1,18 @@
 ; 6502 - Colour grid program
 
 ;; Initialize the registers
-LDX #$00 ;  Put 0 into the X register
+LDX #$00 ; Put 0 into the X register
 LDA #$02 ; Put 2 into the A register
+STA $01  ; Store A to the second zero-page index
+LDA #$00 ; Put 0 into the A register
+STA $00  ; Store A to the first zero-page index 
 
 ;; Run the program
-STA $01  ; Store A to the second zero-page index
 LDA $fe  ; Load random colour into the A register
 STA ($00,X)
 LDY #$ff
 
-CPY $00
+CPY $00 
 BNE IncBig
 
 INC $01
@@ -22,8 +24,8 @@ LDA #$02
 STA $01
 
 NotOverwriting:
-JMP $0606
+JMP $60A
 
 IncBig:
 INC $00
-JMP $0606
+JMP $60A
