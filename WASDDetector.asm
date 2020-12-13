4df038a0f867ab1ff27db61f2424e3b4c49ba3f2
Sebastian Ouellette <bigbrosebas@gmail.com>
2020-12-13 16:48:57 -0500

    Update WASDDetector.asm
    Now fills the whole screen

--- WASDDetector.asm
+++ WASDDetector.asm
@@ -1,6 +1,12 @@
 ;; 6502 W/A/S/D detector
 
-LDX #$00
+LDA #0     ; Set the lower byte
+STA $01
+LDA #2     ; Set the higher byte
+STA $02
+LDX #6     ; Set X to immediate 0
+LDY #$00
+
 
 Loop:
 LDA $ff      ; Load the last pressed key into A
@@ -28,24 +34,33 @@ JMP Loop     ; Listen for another key
 ;; Functions
 GoingUp:
 LDA #$2
-STA $0200, X
-INX
-JMP Loop     ; Listen for another key
+STA ($01), Y
+JMP Increment     ; Increment the pointer properly
 
 GoingLeft:
 LDA #$7
-STA $0200, X
-INX
-JMP Loop     ; Listen for another key
+JMP Increment     ; Increment the pointer properly
 
 GoingDown:
 LDA #$5
-STA $0200, X
-INX
-JMP Loop     ; Listen for another key
+JMP Increment     ; Increment the pointer properly
 
 GoingRight:
 LDA #$6
-STA $0200, X
-INX
-JMP Loop     ; Listen for another key
+;                   Increment the pointer properly
+
+Increment:
+STA ($01), Y ; Store the colour into the GPU
+
+INC $01      ; Increment the lower address byte
+CPY $01      ; Check if the lower byte is immediate 0
+BNE Loop     ; If not, restart loop
+INC $02      ; If so, increment higher address byte
+
+CPX $02      ; Check if the higher byte is immediate 6
+BNE Loop     ; If not, restart loop
+LDY #$02
+STY $02      ; If so, reset it to immediate 2
+LDY #$00
+
+BEQ Loop     ; Restart loop
