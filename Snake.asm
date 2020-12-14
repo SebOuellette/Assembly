b515e7a3c86076948b05f78a292cbdc0c12f5232
Sebastian Ouellette <bigbrosebas@gmail.com>
2020-12-13 19:30:25 -0500

    Fully movable character

--- Snake.asm
+++ Snake.asm
@@ -1,4 +1,4 @@
-;; 6502 W/A/S/D detector
+;; 6502 Snake game
 
 LDA #0     ; Set the lower byte
 STA $01
@@ -9,14 +9,18 @@ LDX #6     ; Set X to immediate 0
 
 Loop:
 LDY #0       ; Reset Y back to 0
-LDA $ff      ; Load the last pressed key into A
 
 ;; Handle the loop counter
 INC $3       ; Game loop delay
+LDA $3       ; Load the delay counter into A
+AND #$3f     ; Only worry about the 0011 1110 bits
+STA $3
 CPY $3       ; Check if loop counter is 0
 BNE Loop     ; If not equal, restart loop
 INC $4       ; If so, increment the second loop delay
 ;; Loop counter is temporary, to view the framerate
+
+LDA $ff      ; Load the last pressed key into A
 
 ;; Check the game controls
 CMP #$77     ; Up (Idk how variables work yet)
@@ -65,22 +69,47 @@ STA ($01), Y ; Store the colour into the GPU
 JMP Loop     ; Restart loop
 
 GoingDown:
-LDA #$5
+LDA #0
+STA ($01), Y ; Clear old position
+LDA $01      ; Load the lower byte into A
+CLC
+ADC #$20     ; Move up one unit on the screen
+STA $01      ; Store new position
+BCC Wrap3    ; Need to decrement the higher byte
+JSR IncrementHigher
+Wrap3:
+LDA #$3      ; Make the box cyan
 STA ($01), Y ; Store the colour into the GPU
 JMP Loop     ; Restart loop
 
 GoingRight:
-LDA #$6
+LDA #$0
+STA ($01), Y ; Clear old position
+LDA $01      ; Load the lower byte into A
+AND #$3f     ; Only worry about the 0011 1111 bits
+CMP #$1f     ; Check if the box is on the right
+BNE Wrap4    ; If not, continue
+SEC
+SBC #$1f     ; If so, move to the left side of the screen
+STA $1
+Wrap4:
+INC $01      ; Move box right
+LDA #$3      ; Make the box cyan
 STA ($01), Y ; Store the colour into the GPU
 JMP Loop     ; Restart loop
 
+
+;; Subroutines
 IncrementHigher:
-INC $02      ; Increment highest
-CPX $02      ; Check if the higher byte is immediate 6
-BNE Loop     ; If not, continue with loop
-LDY #2
-STY $02      ; If so, reset the higher byte back to immediate 2
-JMP Loop     ; Restart loop
+INC $02      ; Decrement highest
+LDY #6
+CPY $02      ; Check if the higher byte is immediate 6
+BNE ReturnDec  ; If not, continue with loop
+LDY #1
+STY $02      ; If so, reset the higher byte back to immediate 1
+ReturnDec:
+LDY #0
+RTS
 
 DecrementHigher:
 DEC $02      ; Decrement highest
